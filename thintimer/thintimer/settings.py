81a15d39a3a7183156e5faada01c12c0b716e165
Greg Barbieri <gfbarbieri@gmail.com>
2023-09-24 19:13:22 -0400

    Updated ALLOWED_HOSTS in settings.py.

--- thintimer/thintimer/settings.py
+++ thintimer/thintimer/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 # Application definition
 

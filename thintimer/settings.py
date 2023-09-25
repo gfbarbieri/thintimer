cf1574a4556698ba12de75d2fa5d07aab5e39788
Greg Barbieri <gfbarbieri@gmail.com>
2023-09-25 10:57:41 -0400

    Update settings.

--- thintimer/settings.py
+++ thintimer/settings.py
@@ -85,6 +85,10 @@ WSGI_APPLICATION = 'thintimer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+# }
+
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
